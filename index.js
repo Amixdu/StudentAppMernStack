4296576faf8f9a430d439ed77bd561b3ab1f8a77
Amixdu <amixxdu@gmail.com>
2022-08-06 21:12:53 +0530

    create and login route added with JWT

--- index.js
+++ index.js
@@ -1,15 +1,52 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/user.model')
+const jwt = require('jsonwebtoken')
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/api/create', (req, res) => {
+mongoose.connect('mongodb://localhost:27017/internship-assignment')
+
+app.post('/api/create', async (req, res) => {
     console.log(req.body)
-    res.json({ status: 'ok' })
+    try{
+        const user = await User.create({
+            email: req.body.email
+        })
+        res.json({ status: 'success' })
+        // res.send(user)
+    } 
+    catch{
+        res.json({ status: 'error' })
+    }
 })
+
+
+
+app.post('/api/login', async (req, res) => {
+    const user = await User.findOne({ email: req.body.email })
+    
+
+    if (user) {
+        console.log("YES")
+        const token = jwt.sign({
+            email: req.body.email
+        }, process.env.ACCESS_TOKEN)
+
+        return res.json({ status: 'success', user: token})
+    }
+    else{
+        console.log("NO")
+        return res.json({ status: 'error' })
+    }
+    
+})
+
+
 
 app.listen(8000, () => {
     console.log("Started on: http://localhost:8000/")
---
+++ models/user.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const User = new Schema(
+    {
+        email: { type: String, required: true, unique: true }
+    },
+    { collections: 'users'}
+)
+
+const model = mongoose.model('Users', User)
+
+module.exports = model
