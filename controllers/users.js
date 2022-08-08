8f54bac5d84114ff014d15b1cb381b03d188e98c
Amixdu <amixxdu@gmail.com>
2022-08-08 15:14:42 +0530

    User detail update route complete

--- controllers/users.js
+++ controllers/users.js
@@ -26,16 +26,44 @@ export const createUser = async (req, res) => {
 }
 
 export const loginUser = async (req, res) => {
-    const user = await User.findOne({ email: req.body.email, password: req.body.password })
-    if (user) {
+    const userData = await User.findOne({ email: req.body.email, password: req.body.password })
+    if (userData) {
         console.log("YES")
         const user = { email: req.body.email }
         const token = jwt.sign(user, process.env.ACCESS_TOKEN)
 
-        return res.json({ status: 'success', user: token})
+        return res.json({ status: 'success', user: token, userStatus: userData.status })
     }
     else{
         console.log("NO")
+        return res.json({ status: 'error' })
+    }
+}
+
+
+export const updateUser = async (req, res) => {
+    const token = req.headers['x-access-token']
+
+    try{
+        const user = jwt.verify(token, process.env.ACCESS_TOKEN)
+        const email = user.email
+        const userData = await User.findOne({ email: email })
+        await User.updateOne({ email: email }, { $set: 
+            { 
+                firstName: req.body.firstName, 
+                lastName: req.body.lastName, 
+                email: email,   
+                dateOfBirth: req.body.dateOfBirth,
+                mobile: req.body.mobile,
+                status: false,
+                password: userData.password,
+                accountType: userData.accountType
+            }})
+        console.log('User Details Updated')
+        return res.json({ status: 'ok' })
+    }
+    catch(error){
+        console.log(error)
         return res.json({ status: 'error' })
     }
 }
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 import express from 'express'
 
-import { createUser, loginUser } from '../controllers/users.js'
+import { createUser, loginUser, updateUser } from '../controllers/users.js'
 
 const router = express.Router()
 
 router.post('/create', createUser)
 router.post('/login', loginUser)
+router.post('/update', updateUser)
 
 export default router
